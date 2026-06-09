46cdbac48d1bc2cb4fea2bd2a9c43a0c8eba2881
NeO <neo@MacBook-Pro-NeO.local>
2026-06-09 12:40:31 +0300

    feat: update routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,31 @@
-import { Routes, Route } from 'react-router-dom'
-// Pages
-import AuthPage      from './pages/AuthPage'
-import TeamGame      from './pages/team/TeamGame'
-import TeamResults   from './pages/team/TeamResults'
-import AdminDashboard from './pages/admin/AdminDashboard'
-import AdminGames    from './pages/admin/AdminGames'
-import AdminGameEdit from './pages/admin/AdminGameEdit'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import AuthPage        from './pages/AuthPage'
+import AdminLogin      from './pages/admin/AdminLogin'
+import AdminDashboard  from './pages/admin/AdminDashboard'
+import AdminGames      from './pages/admin/AdminGames'
+import AdminGameEdit   from './pages/admin/AdminGameEdit'
+import Lobby           from './pages/team/Lobby'
+import TeamGame        from './pages/team/TeamGame'
+import TeamResults     from './pages/team/TeamResults'
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/"               element={<AuthPage />} />
-      <Route path="/game"           element={<TeamGame />} />
-      <Route path="/results"        element={<TeamResults />} />
-      <Route path="/admin"          element={<AdminDashboard />} />
-      <Route path="/admin/games"    element={<AdminGames />} />
-      <Route path="/admin/games/:id" element={<AdminGameEdit />} />
+      {/* Капитан */}
+      <Route path="/agt"          element={<AuthPage />} />
+      <Route path="/agt/lobby"    element={<Lobby />} />
+      <Route path="/agt/game"     element={<TeamGame />} />
+      <Route path="/agt/results"  element={<TeamResults />} />
+
+      {/* Организатор */}
+      <Route path="/agt/adm"           element={<AdminLogin />} />
+      <Route path="/agt/adm/dashboard" element={<AdminDashboard />} />
+      <Route path="/agt/adm/games"     element={<AdminGames />} />
+      <Route path="/agt/adm/games/:id" element={<AdminGameEdit />} />
+
+      {/* Редиректы */}
+      <Route path="/"   element={<Navigate to="/agt" replace />} />
+      <Route path="*"   element={<Navigate to="/agt" replace />} />
     </Routes>
   )
 }

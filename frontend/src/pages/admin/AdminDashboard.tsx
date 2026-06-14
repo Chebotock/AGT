9ae0d758b541ba6f78baaccb5b93958bc7ba4731
NeO <neo@MacBook-Pro-NeO.local>
2026-06-14 17:09:24 +0300

    fix: remove extra closing div

--- frontend/src/pages/admin/AdminDashboard.tsx
+++ frontend/src/pages/admin/AdminDashboard.tsx
@@ -246,7 +246,6 @@ export default function AdminDashboard() {
                     </div>
                   )
                 })()}
-                </div>
               </div>
             ))}
           </div>
